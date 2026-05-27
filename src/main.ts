4c5d9932f6794916c0697b59f2bd4b66ac633a60
AlexandruGabi <constantinescu.alexandrugabi47@gmail.com>
2026-05-27 19:06:45 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<section id="center">
+  <canvas id="canvas" width="400" height="400"></canvas>
+</section>
+`
+
